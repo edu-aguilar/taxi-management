b024fbae3260947130ad7a9ab0f0521574409fe9
Eduardo Aguilar <eduardoaguilartirado@gmail.com>
2017-04-14 11:46:59 +0200

    [FEAT]: journey POST is now working

--- backend/controllers/journey.controller.js
+++ backend/controllers/journey.controller.js
@@ -3,8 +3,13 @@ var Journey = require('../models/journey.js');
 exports.newJourney = newJourney;
 
 function newJourney(req, res, next) {
+    console.log(req.body);
     var journey = new Journey();
     journey.id = req.body.id;
+    journey.startingKm = req.body.startingKm;
+    journey.finishingKm = req.body.finishingKm;
+    journey.startinglDate = req.body.startinglDate;
+    journey.finishingDate = req.body.finishingDate;
 
     journey.save(function(err) {
         if (err){
@@ -12,5 +17,4 @@ function newJourney(req, res, next) {
         }
         res.json({ message: 'Journey created!' });
     });
-
 }
--- backend/models/journey.js
+++ backend/models/journey.js
@@ -1,10 +1,11 @@
 var mongoose     = require('mongoose');
+mongoose.Promise = global.Promise;
 var Schema       = mongoose.Schema;
 var RideSchema   = require('mongoose').model('Ride').schema;
 
 var JourneySchema   = new Schema({
     id: { type: String, required: true },
-    startinglKm: { type: String, required: true },
+    startingKm: { type: String, required: true },
     finishingKm: { type: String, required: true },
     startinglDate: { type: String, required: true },
     finishingDate: { type: String, required: true },
